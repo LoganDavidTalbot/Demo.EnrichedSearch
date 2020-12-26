8102e0009520f2632d38627c9089bf73340b59a4
logandaivdtalbot <logan.talbot@capgemini.com>
2020-12-26 23:21:52 +0000

    Add more appsettings

--- Demo.EnrichedSearch.IaC/Powershell/Add-AppSettings.ps1
+++ Demo.EnrichedSearch.IaC/Powershell/Add-AppSettings.ps1
@@ -2,12 +2,28 @@
     [string]$ResourceGroupName,
     [string]$WebAppName,
     [string]$SearchServiceName,
-    [string]$StorageAccountName
+    [string]$StorageAccountName,
+    [string]$CognitiveServiceName
 )
 Install-Module -Name Az.Search -force
 Install-Module -Name Az.Websites -force
 
+# Get Search Service Key
 $searchServiceKeys = Get-AzSearchAdminKeyPair -ResourceGroupName $ResourceGroupName -ServiceName $SearchServiceName
 
-$newAppSettings = @{"SearchService_ServiceName"=$SearchServiceName;"SearchService_ApiKey"=$searchServiceKeys.Primary;}
+# Get Storage Account String
+$sa = Get-AzStorageAccount -StorageAccountName $StorageAccountName -ResourceGroupName $ResourceGroupName
+$saKey = (Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $saName)[0].Value
+$saConnectionString = 'DefaultEndpointsProtocol=https;AccountName=' + $StorageAccountName + ';AccountKey=' + $saKey + ';EndpointSuffix=core.windows.net' 
+
+# Get CognitiveService Key
+$cognitiveServiceKey = Get-AzCognitiveServicesAccountKey -ResourceGroupName $ResourceGroupName -name $CognitiveServiceName
+
+# Add App Settings to Web App
+$newAppSettings = @{
+    "SearchService_ServiceName"=$SearchServiceName;
+    "SearchService_ApiKey"=$searchServiceKeys.Primary;
+    "AzureBlobConnectionString"=$saConnectionString;
+    "CognitiveServiceKey"=$cognitiveServiceKey;
+}
 Set-AzWebApp -AppSettings $newAppSettings -Name $WebAppName -ResourceGroupName $ResourceGroupName
